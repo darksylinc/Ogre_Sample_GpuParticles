e7aaf0868f787222c0e1f30143a3f468bc5d540f
przemir <przemyslaw.bagard@gmail.com>
2023-01-21 11:31:50 +0100

    Remove unnecessary code concerning particle_enabled property.

--- media/Hlms/Particle/GLSL/500.Structs_piece_vs_piece_ps.glsl
+++ media/Hlms/Particle/GLSL/500.Structs_piece_vs_piece_ps.glsl
@@ -33,53 +33,6 @@
 	};
 @end
 
-@property(particle_enabled)
-	struct Particle
-	{
-		float3 pos;
-		float rot;
-		float4 colour;
-		float2 size;
-		float spriteNumber;
-		// float anything;
-	};
-
-
-	layout(std430, ogre_R5) readonly restrict buffer particleDataListBuf
-	{
-		Particle particleDataList[];
-	};
-
-	struct EmitterInstanceData {
-		float4x4 emitterLocation;
-		float elapsedTime;
-
-		uint particleArrayStartIndex;
-
-		/// Particles count without new particles
-		uint particlesCount;
-
-		uint newParticles;
-
-		uint MaxParticles;
-		uint randomIteration;
-	};
-
-
-	layout(std430, ogre_R4) readonly restrict buffer emitterInstanceDataBuf
-	{
-		EmitterInstanceData emitterInstanceData[];
-	};
-
-	const float2 QuadCorners[] =
-	{
-		float2(0, 0),
-		float2(1, 0),
-		float2(1, 1),
-		float2(0, 1),
-	};
-@end
-
 @end
 
 
--- media/Hlms/Particle/HLSL/500.Structs_piece_vs_piece_ps.hlsl
+++ media/Hlms/Particle/HLSL/500.Structs_piece_vs_piece_ps.hlsl
@@ -16,43 +16,6 @@ static const float2 QuadCorners[] = {
 };
 @end
 
-@property(particle_enabled)
-struct Particle {
-float3 pos;
-float rot;
-float4 colour;
-float2 size;
-float spriteNumber;
-// float anything;
-};
-
-StructuredBuffer<Particle> particleDataList : register(t5);
-
-struct EmitterInstanceData {
-    float4x4 emitterLocation;
-    float elapsedTime;
-    
-    uint particleArrayStartIndex;
-    
-    /// Particles count without new particles
-    uint particlesCount;
-    
-    uint newParticles;
-    
-    uint MaxParticles;
-    uint randomIteration;
-};
-
-StructuredBuffer<EmitterInstanceData> emitterInstanceData : register(t4);
-
-static const float2 QuadCorners[] = {
-	float2(0, 0),
-	float2(1, 0),
-	float2(1, 1),
-	float2(0, 1),
-};
-
-@end
 @end
 
 
